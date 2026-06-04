0673f922966d9640c48481f475d8aedead72f249
Tsjippy <ewaldha@hotmail.com>
2026-06-04 20:36:30 +0200

    fixed: spacing problem

--- tsjippy-welcome-message.php
+++ tsjippy-welcome-message.php
@@ -30,7 +30,7 @@ if (! defined('ABSPATH')) {
 define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
 define(__NAMESPACE__ . '\PLUGINPATH', __DIR__ . '/');
 define(__NAMESPACE__ . '\PLUGINVERSION', get_plugin_data(__FILE__, false, false)['Version']);
-define(__NAMESPACE__ . '\PLUGINSLUG', str_replace('tsjippy-', '', basename(__FILE__, ' .php')));
+define(__NAMESPACE__ . '\PLUGINSLUG', str_replace('tsjippy-', '', basename(__FILE__, '.php')));
 define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_' . PLUGINSLUG . '_settings', []));
 
 
