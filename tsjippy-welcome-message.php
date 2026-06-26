9677085ddebac5fa5c1457f7326a786c05e943cd
Tsjippy <ewaldha@hotmail.com>
2026-06-26 12:40:25 +0200

    changed: plugin tested up to 7.0

--- tsjippy-welcome-message.php
+++ tsjippy-welcome-message.php
@@ -10,7 +10,7 @@ namespace TSJIPPY\WELCOMEMESSAGE;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         6.9
+ * Tested up to:         7.0
  * Plugin URI:            https://github.com/Tsjippy/welcomemessage
  * Tested:                6.9
  * TextDomain:            tsjippy
