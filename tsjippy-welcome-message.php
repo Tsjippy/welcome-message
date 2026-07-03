3037169f4a152a45ea0db351e32d4c1f67b94fc2
Tsjippy <ewaldha@hotmail.com>
2026-07-03 18:59:39 +0200

    fixed: plugin settings key

--- tsjippy-welcome-message.php
+++ tsjippy-welcome-message.php
@@ -34,7 +34,7 @@ define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
 define(__NAMESPACE__ . '\PLUGINPATH', __DIR__ . '/');
 define(__NAMESPACE__ . '\PLUGINVERSION', get_plugin_data(__FILE__, false, false)['Version']);
 define(__NAMESPACE__ . '\PLUGINSLUG', str_replace('tsjippy-', '', basename(__FILE__, '.php')));
-define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_' . PLUGINSLUG . '_settings', []));
+define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_welcome-message_settings', []));
 
 // run right before activation
 register_activation_hook(__FILE__, function () {
