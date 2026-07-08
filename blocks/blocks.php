6e12ad4c3928fbbad0f4dfd544bb0679805d4539
Tsjippy <ewaldha@hotmail.com>
2026-07-08 15:21:27 +0200

    added: welcome message block

---
+++ blocks/blocks.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TSJIPPY\WELCOMEMESSAGE;
+
+if (! defined('ABSPATH')) exit;
+
+add_action('init', __NAMESPACE__ . '\blockInit');
+function blockInit()
+{
+    register_block_type(
+        'tsjippy-welcome-message/show_message',
+        array(
+            'title'            => __( 'Show Welcome Message', 'tsjippy' ),
+            'render_callback'  => __NAMESPACE__ . '\welcomeMessage',
+            'supports'         => array(
+                'autoRegister' => true,
+            ),
+        )
+    );
+}
